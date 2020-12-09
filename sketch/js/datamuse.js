97d837ff71c3a7cf98103250e1e1b4a65a37e81e
cqx931 <cqx931@gmail.com>
2020-12-09 13:44:38 +0100

    minor tweaks

--- sketch/js/datamuse.js
+++ sketch/js/datamuse.js
@@ -9,8 +9,8 @@ const localStorageLocation = "../localStorage/section2/";
 
 // Urls for each destinations
 const urlOptions = {
-  "pythonAnywhere": "http://cqx931.pythonanywhere.com",
-  "localServer": "http://127.0.0.1:5000"
+  "pythonAnywhere": "https://cqx931.pythonanywhere.com",
+  "localServer": "https://127.0.0.1:5000"
 }
 
 function datamuse(params, plant, callback) {
